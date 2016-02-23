f5c08a43a26515c7444e47ccea03ea82a3465311
Andy Potanin <andy.potanin@usabilitydynamics.com>
2016-02-23 20:53:50 +0200

    separate post creation and updating examples, added ability to set post author via env variable for testing

--- examples/update.js
+++ examples/update.js
@@ -14,12 +14,14 @@ function insertCallback(error, data) {
   console.log(require('util').inspect(error ? error.message : data, { showHidden: true, colors: true, depth: 2 }));
 }
 
-// Create New Post.
+// Update Existing Post.
 client.insertPost({
+  ID: 1,
+  if_not_modified_since: new Date(( new Date() ).getTime() - ( 60 * 1000 )),
   post_type: 'post',
   post_status: 'draft',
-  post_title: 'Test Post added using WordPress Client',
-  post_author: process.env.WORDPRESS_AUTHOR_ID || 1,
+  post_title: 'Test Post',
+  post_author: 1,
   post_excerpt: 'Summary of post',
   post_content: '<b>Rich-text</b> detail of of post.',
   post_modified_gmt: new Date(),
@@ -34,37 +36,36 @@ client.insertPost({
       value: 'xml-rpc'
     },
     {
-      key: 'key1',
-      value: 'value1'
-    },
+      key: 'some-key',
+      value: 'some-value'
+    }
+  ],
+  terms_names: {
+    category: [ 'Awesome Category', 'Another Category' ],
+    post_tag: [ 'Chicago' ]
+  }
+}, insertCallback);
+
+// Update non-existing post, should fail.
+client.insertPost({
+  ID: 23432324321,
+  if_not_modified_since: new Date(( new Date() ).getTime() - ( 60 * 1000 )),
+  post_type: 'post',
+  post_status: 'draft',
+  post_title: 'Test Post',
+  post_author: 1,
+  post_excerpt: 'Summary of post',
+  post_content: '<b>Rich-text</b> detail of of post.',
+  post_modified_gmt: new Date(),
+  post_date: new Date(),
+  post_format: 'standard',
+  comment_status: 'closed',
+  ping_status: 'closed',
+  sticky: false,
+  custom_fields: [
     {
-      key: 'key2',
-      value: 'value2'
-    },
-    {
-      key: 'key_3',
-      value: 'value_3'
-    },
-    // the following will not work because meta keys can not start with underscores
-    {
-      key: '_key_4',
-      value: 'value_4'
-    },
-    {
-      key: 'key:5',
-      value: 'value.5'
-    },
-    {
-      key: 'key::6',
-      value: 'value.6'
-    },
-    {
-      key: 'key-7',
-      value: 'value.7 - first'
-    },
-    {
-      key: 'key-7',
-      value: 'value.7 - second'
+      key: '_source',
+      value: 'xml-rpc'
     },
     {
       key: 'some-key',
@@ -76,3 +77,4 @@ client.insertPost({
     post_tag: [ 'Chicago' ]
   }
 }, insertCallback);
+
